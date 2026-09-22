b76cf5320f1498f51bb53711ca60650dbeeb341e
Delzon <1085593+Delzon@users.noreply.github.com>
2026-09-22 17:34:29 -0500

    up

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const axios = require('axios');
+const WebSocket = require('ws');
 const { makeWASocket, useMultiFileAuthState } = require('@whiskeysockets/baileys');
 const { createClient } = require('@supabase/supabase-js');
 const { TelegramClient } = require('teleproto');
